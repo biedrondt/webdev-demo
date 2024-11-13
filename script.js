9bb7b59f7f80422582fc36a1121b74ee5b4faa9e
Daniel Biedron <dbiedron@email.wm.edu>
2024-11-12 21:41:41 -0500

    Completed tutorial.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
 
+function addTask(){
+
+    /*here we need to access the content of the input and do something with it*/
+
+    const taskInput = document.getElementById("taskInput").value;
+
+    if (taskInput) {
+        
+        const newTask = document.createElement('li');
+        newTask.innerText = taskInput;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className="button";
+        deleteButton.innerText="x";
+        newTask.appendChild(deleteButton)
+
+        deleteButton.addEventListener("click", () => taskList.removeChild(newTask));
+
+        document.getElementById("taskList").appendChild(newTask);    
+
+        document.getElementById('taskInput').value=''
+    }
+}
